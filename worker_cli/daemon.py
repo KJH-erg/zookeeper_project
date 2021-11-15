fcff9e18efaae0c6fd895d50bb499e9e9169b0d4
KJH-erg <kjh97507@gmail.com>
2021-11-15 22:53:15 +0900

    add inputs and distribute to workers day1

--- worker_cli/daemon.py
+++ worker_cli/daemon.py
@@ -1,10 +1,16 @@
 import asyncio
+import random
 import time
 from kazoo.client import KazooState
 from kazoo.client import KazooClient
 import sys
 import logics
 import socket
+from kazoo.retry import KazooRetry
+from kazoo.recipe.watchers import PatientChildrenWatch
+import time
+import kazoo.exceptions
+import run
 members = []
 ip_table ={
     '10.178.0.3':'1',
@@ -12,8 +18,15 @@ ip_table ={
     '10.178.0.3':'3',
     '10.178.0.3':'4',
     '10.178.0.3':'5',
-    '172.30.1.58':'local'
+    '172.30.1.58':'local1'
 }
+def my_callback(async_obj):
+    print('enter')
+    try:
+        children = async_obj.get()
+        print(children)
+    except:
+        print('error')
 def membership(flag, input):
     if flag =='add':
         members.append(input)
@@ -23,15 +36,44 @@ def membership(flag, input):
         pass
     return members
 
-  
-async def shell_input(flag,zk,input_lock):
-    
+
+
+async def execute_run(zk,worker,children):
     while True:
-        var =input()
-        if var:
-            logics.main(var,zk,input_lock)
-            break
-    flag.set()
+        children = zk.get_children('/inputs')
+        if len(children) == 0:
+            print('no inputs anymore wait for inputs')
+            watcher = PatientChildrenWatch(zk, '/inputs',
+                               time_boundary=20)
+            async_object = watcher.start()
+            children, child_async = async_object.get()
+        else:
+            i = random.choice(children)
+            try:
+                lock = zk.Lock("/inputs/"+str(i), worker)
+                lock.acquire(timeout=5)
+            except kazoo.exceptions.LockTimeout:
+                print(str(i)+' is passed')
+                continue
+            data, stat = zk.get("/inputs/"+str(i))
+            print('finished'+str(i))
+            print("Version: %s, data: %s" % (stat.version, data.decode("utf-8")))
+            flag = asyncio.Event()
+            asyncio.create_task(run.main(flag,data))
+            await flag.wait()
+            zk.create("/result/"+str(i))
+            
+            async_obj = zk.delete_async("/inputs/"+str(i))
+            async_obj.rawlink(my_callback)
+            lock.release()
+            
+            
+            
+        
+            
+
+
+        
 
 
 async def main(ip):
@@ -51,13 +93,11 @@ async def main(ip):
     membership_lock= zk.Lock('/members')
     #get initial members in zkserver
 
-    membership_lock.acquire(timeout=100000)
+    
     zk.create('/members/'+worker_id,ephemeral=True)
-    membership_lock.release()
+    
 
-    @zk.ChildrenWatch("/inputs/")
-    def input_watch(children):
-        print(children)
+    
     
     #watch for membership
     # @zk.ChildrenWatch("/members")
@@ -77,9 +117,14 @@ async def main(ip):
     #     logics.remove_completed_input()
     
     while True:
+        children = zk.get_children('/inputs')
         flag = asyncio.Event()
-        asyncio.create_task(shell_input(flag,zk,membership_lock))
+        asyncio.create_task(execute_run(zk,worker_id,children))
         await flag.wait()
+
+    
+
+        
 
 
 
---
+++ worker_cli/run.py
@@ -0,0 +1,6 @@
+import asyncio
+import time
+async def main(flag,i):
+    print('wow')
+    time.sleep(10)
+    flag.set()
